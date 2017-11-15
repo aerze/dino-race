7053ef4dc65171bf5e4d2c23bc8367c49d5c85ff
aerze <gus@mythril.co>
2017-11-14 17:13:03 -0800

    add assets and correct scaling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser-ce'
 import MainState from './state'
 
-const game = new Phaser.Game(400, 450)
-game.state.add('main', MainState(game))
+const game = new Phaser.Game(126, 126, Phaser.AUTO, 'canvas')
+game.state.add('main', new MainState(game))
 game.state.start('main')
--- src/state.js
+++ src/state.js
@@ -1,3 +1,5 @@
+import Phaser from 'phaser-ce'
+
 class MainState {
   /**
    * Creates new state
@@ -9,10 +11,17 @@ class MainState {
 
   preload () {
     // load spritesheet
+    this.game.load.atlasJSONHash('vita', '/assets/dino/vita.png', '/assets/dino/vita.json')
+    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE
+    this.game.scale.setUserScale(4, 4)
+    this.game.renderer.renderSession.roundPixels = true
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas)
   }
 
   create () {
     // create player and animations
+    this.game.stage.backgroundColor = '#534D41'
+    const vita = this.game.add.sprite(0, 0, 'vita')
   }
 
   update () {
