062c3e36cd7cba02103861a237de12eb75e0fdec
aerze <gus@mythril.co>
2017-11-14 17:55:46 -0800

    add update logic

--- src/state.js
+++ src/state.js
@@ -21,20 +21,48 @@ class MainState {
   create () {
     // create player and animations
     this.game.stage.backgroundColor = '#534D41'
-    const vita = this.game.add.sprite(0, 0, 'vita')
+    this.vita = this.game.add.sprite(0, 0, 'vita')
+    this.vita.anchor.set(0.5, 0.5)
 
-    vita.animations.add('idle', [0, 1, 2, 3], 10, true)
-    vita.animations.add('move', [4, 5, 6, 7, 8, 9], 10, true)
-    vita.animations.add('kick', [10, 11, 12], 10, true)
-    vita.animations.add('hurt', [13, 14, 15, 16], 10, true)
-    vita.animations.add('crouch', [17], 10, true)
-    vita.animations.add('sneak', [18, 19, 20, 21, 22, 23], 10, true)
+    this.vita.animations.add('idle', [0, 1, 2, 3], 15, true)
+    this.vita.animations.add('move', [4, 5, 6, 7, 8, 9], 16, true)
+    this.vita.animations.add('kick', [10, 11, 12], 10, true)
+    this.vita.animations.add('hurt', [13, 14, 15, 16], 10, true)
+    this.vita.animations.add('crouch', [17], 10, true)
+    this.vita.animations.add('sneak', [18, 19, 20, 21, 22, 23], 10, true)
 
-    vita.animations.play('move')
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.game.physics.arcade.gravity.y = 300
+
+    this.game.physics.enable(this.vita, Phaser.Physics.ARCADE)
+
+    this.vita.body.collideWorldBounds = true
+    this.vita.body.gravity.y = 1000
+    this.vita.body.maxVelocity.y = 500
+
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+    this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
   }
 
   update () {
     // handle button logic
+    this.vita.body.velocity.x = 0
+
+    if (this.cursors.left.isDown) {
+      this.vita.body.velocity.x = -150
+      this.vita.scale.x = -1
+      this.vita.animations.play('move')
+    } else if (this.cursors.right.isDown) {
+      this.vita.body.velocity.x = 150
+      this.vita.scale.x = 1
+      this.vita.animations.play('move')
+    } else {
+      this.vita.animations.play('idle')
+    }
+
+    if (this.jumpButton.isDown && this.vita.body.onFloor()) {
+      this.vita.body.velocity.y = -500
+    }
   }
 }
 
