ab614c78ef840976128cba8b436713431093a764
Simon Prickett <simon@crudworks.org>
2017-11-14 19:58:45 -0800

    Fixes size from 126 x 126 to 128 x 128

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser-ce'
 import MainState from './state'
 
-const game = new Phaser.Game(126, 126, Phaser.AUTO, 'canvas')
+const game = new Phaser.Game(128, 128, Phaser.AUTO, 'canvas')
 game.state.add('main', new MainState(game))
 game.state.start('main')
